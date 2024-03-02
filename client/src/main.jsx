26b77c8535d23005531d33064c3f7b620c01ea74
Shubham Singhvi <shubhamsinghvi54@gmail.com>
2024-03-02 22:44:00 +0530

    Added PWA

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import LoginContextProvider from "./context/LoginContext.jsx";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,3 +11,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </LoginContextProvider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
