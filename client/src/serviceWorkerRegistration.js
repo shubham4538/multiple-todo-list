2d32b74834952029d1d82584e6d7c693dd6da76a
Shubham Singhvi <shubhamsinghvi54@gmail.com>
2024-03-05 09:14:11 +0530

    Update serviceWorkerRegistration.js

--- client/src/serviceWorkerRegistration.js
+++ client/src/serviceWorkerRegistration.js
@@ -14,7 +14,8 @@ export function register(config) {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/src/service-worker.js`;
+      const swUrl =
+        "https://multiple-todo-list-front.vercel.app/src/service-worker.js";
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
