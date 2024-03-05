61e2def95a8bbc322c009f9332612b084f2355b9
Shubham Singhvi <shubhamsinghvi54@gmail.com>
2024-03-05 22:51:26 +0530

    hope this works

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import LoginContextProvider from "./context/LoginContext.jsx";
-import * as serviceWorkerRegistration from "../serviceWorkerRegistration.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,5 +10,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </LoginContextProvider>
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
