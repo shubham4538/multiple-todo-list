136ebbbfc95545aa494392cd8c0c34947e43e681
Shubham Singhvi <83571017+shubham4538@users.noreply.github.com>
2024-03-05 15:20:14 +0530

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import LoginContextProvider from "./context/LoginContext.jsx";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
+import * as serviceWorkerRegistration from "../serviceWorkerRegistration.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
